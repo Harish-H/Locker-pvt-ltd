eb0978afb836f2f52c7658ed3f58f4e255fa6d6b
Harish_H1 <Harish_H1@DELL.COM>
2021-03-28 14:30:21 +0530

    Locker pvt ltd project source code

---
+++ src/org/lockers/com/Welcome.java
@@ -0,0 +1,18 @@
+package org.lockers.com;
+
+public class Welcome {
+	public static void main(String[] args) {
+		//Welcome Page
+		System.out.println("Welcome to Lockers.com");
+		System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+		System.out.println("Developed By HARISH H");
+		System.out.println();
+		
+		//Call UserInterface function
+		UserInterface ui=new UserInterface();
+		ui.userInterface();
+		
+		
+	}
+
+}
